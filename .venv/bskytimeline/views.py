9a4ced046c27f83c57c51df636f707c454573cf0
ynack <nakayama.yosuke@gmail.com>
2024-02-17 19:36:09 +0900

    third commit

---
+++ .venv/bskytimeline/views.py
@@ -0,0 +1,13 @@
+from flask import render_template, request
+from bskytimeline import app
+from atproto import Client
+from atproto_client.models.app.bsky.feed.get_timeline import Response
+
+client = Client()
+client.login('ユーザ名', 'パスワード')
+
+@app.route('/')
+def index():
+    res = client.get_timeline()
+
+    return render_template('bskytimeline/bsky_timeline.html', timeline = res)
